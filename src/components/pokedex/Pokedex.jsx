aebbc2a84392d4ff306bcb71013abd58998fcf9a
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2019-08-15 18:50:58 -0300

    finalizando pokedex

--- src/components/pokedex/Pokedex.jsx
+++ src/components/pokedex/Pokedex.jsx
@@ -11,7 +11,8 @@ const headerProps = {
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon'
 const initialState = {
     search: '',
-    pokemon: []
+    pokemon: [],
+    list: []
 }
 
 export default class Pokedex extends Component {
@@ -24,19 +25,17 @@ export default class Pokedex extends Component {
     }
 
     clear() {
-        this.setState({ search: initialState.search, pokemon: initialState.pokemon })
+        this.setState({ search: initialState.search, pokemon: initialState.pokemon, list: initialState.list })
     }
 
     async getPokemon(event) {
-        await this.setState({ search: event.target.value, pokemon: initialState.pokemon });
+        let searchValues = event.target ? event.target.value : event;
+        await this.setState({ search: searchValues, pokemon: initialState.pokemon });
         if(this.state.search.length > 0){
             await axios.get(`${baseUrl}/${this.state.search.toString().toLowerCase()}`).then(async resp => {
                 await this.setState({pokemon: resp.data });
                 await this.showPokemon();
             })
-            .catch(error => {
-                console.log(`${this.state.search} não encontrado!`)
-            });
         }
     }
 
@@ -46,7 +45,7 @@ export default class Pokedex extends Component {
                 <div className="mt-5 p-3">
                     <div className="row">
                         <div className="col-12 col-md-2">
-                            <img className="img-thumbnail cover-poke" src={this.state.pokemon.sprites.front_default} />
+                            <img className="img-thumbnail cover-poke" alt="Imagem pokémon" src={this.state.pokemon.sprites.front_default} />
                         </div>
                         <div className="col-12 col-md-3">
                             <p><b>Informações</b></p>
@@ -71,22 +70,64 @@ export default class Pokedex extends Component {
     }
 
     getStats() {
+        let i = 1;
         return this.state.pokemon.stats.map(stats => {
             return (
-                <p className="mb-2"><b>{stats.stat.name}:</b> {stats.base_stat}</p>
+                <p key={i++} className="mb-2"><b>{stats.stat.name}:</b> {stats.base_stat}</p>
             )
         })
     }
 
     getAbilities() {
+        let i = 1;
         return this.state.pokemon.abilities.map(abilities => {
             return (
-                <p className="mb-2"><b>{abilities.ability.name}</b></p>
+                <p key={i++} className="mb-2"><b>{abilities.ability.name}</b></p>
             )
         })
     }
 
+    async getAll(offsetLimit) {
+        await axios.get([offsetLimit]).then(async resp => {
+            await this.setState({ list: initialState.list })
+            await this.setState({ list: resp.data })
+            await this.showAll()
+        })
+    }
+
+    showAll() {
+        console.log(this.state.list.results)
+        return(
+            <div className="mt-5 p-3">
+                <div className="row">
+                    {this.renderPokeList()}
+                    {this.state.list.results !== undefined ? (
+                        <div className="arrowPokeList">
+                            <span onClick={e => this.getAll(this.state.list.previous)}>{'<<'}</span>
+                            <span onClick={e => this.getAll(this.state.list.next)}>{'>>'}</span>
+                        </div>
+                    ) : ''}
+                </div>
+            </div>
+        )
+    }
+
+    renderPokeList(){
+        if(this.state.list.results){
+
+            return this.state.list.results.map(pokemon => {
+                let pokeId = pokemon.url.split("/");
+                return (
+                    <div key={pokemon.url} className="col-6 col-md-3 pokeListItem p-0">
+                        <p onClick={e => this.getPokemon(pokeId[6])}>{pokemon.name}</p>
+                    </div>
+                )
+            })
+        }
+    }
+
     renderForm() {
+        let urlTodos = `${baseUrl}/?offset=00&limit=40`
         return (
             <div className="p-3 mt-3">
                 <div className="form">
@@ -107,8 +148,14 @@ export default class Pokedex extends Component {
                     <hr />
                     <div className="row">
                         <div className="col-12 d-flex justify-content-end">
+                            
+                            <button className="btn btn-primary"
+                                onClick={e => this.getAll(urlTodos)}>
+                                Listar Todos
+                            </button>
+
                             <button className="btn btn-secondary ml-2"
-                                onClick={e => this.clear(e)}>
+                                onClick={e => this.clear()}>
                                 Limpar
                             </button>
                         </div>
@@ -123,6 +170,7 @@ export default class Pokedex extends Component {
             <Main {...headerProps}>
                 {this.renderForm()}
                 {this.showPokemon()}
+                {this.showAll()}
             </Main>
         )
     }
